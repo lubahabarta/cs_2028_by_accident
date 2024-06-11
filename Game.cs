8666754522a02ddc55987de16db483266e9c9a0d
lubahabarta <lubahabarta@gmail.com>
2024-06-11 20:05:39 +0200

    added loop

--- Game.cs
+++ Game.cs
@@ -1,17 +1,29 @@
+using Microsoft.VisualBasic;
+using System;
+
 namespace _2028;
 
 sealed class Game
 {
+    private bool _running;
+
     private Matrix2d _matrix;
     
     public Game(sbyte size)
     {
         _matrix = new Matrix2d(size);
-        
+        _running = true;
     }
 
     public void Start()
     {
-        _matrix.Print();
+        _Loop();
+    }
+
+    private void _Loop() {
+        while (_running) {
+            _matrix.Print();
+            string s = Console.ReadLine() ?? "";
+        }
     }
 }
